b672e974fb94a1e5e3d88bb5b2f5f1068bdaebba
sin1017 <catmoon1017@gmail.com>
2024-02-25 02:44:50 +0800

    fix: 🐛 test
    ✅ Closes: test

---
+++ src/components/defaultFrome/elementUI/config/formItemList.ts
@@ -0,0 +1,51 @@
+import type { FormItem, Options } from "./type";
+import type { UploadProps, UploadUserFile } from "element-plus";
+
+const cityOptions = ref<Options | []>([]);
+
+export const ItemList: FormItem[] = [
+  {
+    label: "姓名",
+    path: "name",
+    elementTag: "input",
+    placeholder: "請輸入姓名",
+    rules: [
+      {
+        required: true,
+        message: "不可空白",
+        trigger: "change",
+      },
+    ],
+  },
+  {
+    label: "手機",
+    path: "phone",
+    elementTag: "input",
+    placeholder: "請輸入手機號碼",
+    inputMain: 10,
+    inputMax: 10,
+    inputType: "number",
+    rules: [
+      {
+        required: true,
+        message: "不可空白",
+        trigger: "change",
+      },
+    ],
+  },
+  {
+    label: "地址",
+    path: "address",
+    elementTag: "group",
+    children: [
+      {
+        label: null,
+        elementTag: "select",
+        path: "city",
+        options: cityOptions,
+      },
+
+    ],
+
+  },
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,52 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
-import vue from '@vitejs/plugin-vue';
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-      imports: [
-        'vue',
-        {
-          'naive-ui': [
-            'useDialog',
-            'useMessage',
-            'useNotification',
-            'useLoadingBar'
-          ]
-        }
-      ]
-    }),
-    Components({
-      resolvers: [
-        ElementPlusResolver(),
-        NaiveUiResolver()
-      ],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(() => {
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        include: [
+          /\.[tj]s?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/, /\.vue\?vue/, // .vue
+          /\.md$/, // .md
+        ],
+        imports: [
+          'vue',
+          // {
+          //   'naive-ui': [
+          //     'useDialog',
+          //     'useMessage',
+          //     'useNotification',
+          //     'useLoadingBar'
+          //   ]
+          // }
+        ],
+        dts: './auto-imports.d.ts',
+        resolvers: [ElementPlusResolver()],
+        eslintrc: {
+          enabled: false,
+          filepath: './.eslintrc-auto-import.json',
+          globalsPropValue: true,
+        },
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver(),
+          NaiveUiResolver()
+        ],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 });
